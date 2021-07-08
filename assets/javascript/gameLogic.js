251ed5f5c1c69aad643953ec0516a04b35cafa80
Weasel <cody.a.tracey@gmail.com>
2021-07-07 21:59:45 -0500

    added in the randomizer to get the current word to guess

--- assets/javascript/gameLogic.js
+++ assets/javascript/gameLogic.js
@@ -1,1 +1,17 @@
-console.log("Connected")
+//Global Variables
+const WORDS = [
+    'stephen strange',
+    'bruce banner',
+    'peter parker',
+    'reed richards',
+    'sue storm',
+];
+let tries = 10;
+//Current word to be guessed
+let currWord;  
+
+//Game Functions
+const randomizer = () => {
+    currWord = WORDS[Math.floor(Math.random()*WORDS.length)].split('')
+};
+
