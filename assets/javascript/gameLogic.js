35b831bc6f8126102b312a5ea5993d6156cc9098
Weasel <cody.a.tracey@gmail.com>
2021-07-07 22:05:24 -0500

    added in function to create the blanks for the mystery word

--- assets/javascript/gameLogic.js
+++ assets/javascript/gameLogic.js
@@ -8,10 +8,24 @@ const WORDS = [
 ];
 let tries = 10;
 //Current word to be guessed
-let currWord;  
+let currWord;
+//The blanks where the letters will be;
+let blanks;  
 
 //Game Functions
 const randomizer = () => {
     currWord = WORDS[Math.floor(Math.random()*WORDS.length)].split('')
 };
+
+const setblanks = () => {
+    let hold = [];
+    currWord.map(i => {
+        if(i === ' ')hold.push(' ');
+        else hold.push('*');
+    })
+    blanks = hold.join("");
+};
+randomizer();
+setblanks();
+console.log(blanks,'\n',currWord.join(''));
 
