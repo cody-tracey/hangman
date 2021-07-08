d59c05ce4d3a85ee6ae7eb2114849909b51fa09b
Weasel <cody.a.tracey@gmail.com>
2021-07-07 22:22:53 -0500

    added in a fucntion to check and see if the game is over, will change from an alert to a modal when I add in the UI component of it

--- assets/javascript/gameLogic.js
+++ assets/javascript/gameLogic.js
@@ -10,7 +10,7 @@ let tries = 10;
 //Current word to be guessed
 let currWord;
 //The blanks where the letters will be;
-let blanks;  
+let blanks;
 //Already guessed chars
 let used = [];
 
@@ -18,14 +18,14 @@ let used = [];
 
 //Picks a random word from the list and assigns it to the currWord variable.
 const randomizer = () => {
-    currWord = WORDS[Math.floor(Math.random()*WORDS.length)].split('')
+    currWord = WORDS[Math.floor(Math.random() * WORDS.length)].split('')
 };
 
 //Creates a series of blanks that is the same length as the currWord variable
 const setblanks = () => {
     let hold = [];
     currWord.map(i => {
-        if(i === ' ')hold.push(' ');
+        if (i === ' ') hold.push(' ');
         else hold.push('*');
     })
     blanks = hold.join("");
@@ -33,15 +33,15 @@ const setblanks = () => {
 
 //Checks to see if the guessed letter is one of the mystery letters, and updates it if it is. If it is not one of the mystery letters it subtracts a try.
 const checker = guessed => {
-    if(!used.includes(guessed)){
-        if(currWord.includes(guessed)){
+    if (!used.includes(guessed)) {
+        if (currWord.includes(guessed)) {
             let hold = blanks.split('');
             currWord.map((item, index) => {
-                if(item === guessed && guessed !== ' ') hold[index] = guessed;
+                if (item === guessed && guessed !== ' ') hold[index] = guessed;
             })
             blanks = hold.join('');
             console.log(blanks)
-        }else{
+        } else {
             tries--;
             console.log(tries)
         };
@@ -51,12 +51,23 @@ const checker = guessed => {
 
 };
 
+//Checks to see if the game is over...
+const endGame = () => {
+    if (tries <= 0) {
+        alert("You have lost :( play again?")
+    }
+    if (!blanks.includes('*')) {
+        alert("You have won :) play again?")
+    }
+};
+
 randomizer();
 setblanks();
-console.log(blanks,'\n',currWord.join(''));
+console.log(blanks, '\n', currWord.join(''));
 
 document.onkeyup = event => {
     let guessed = event.key.toLowerCase();
     checker(guessed);
-}; 
+    endGame();
+};
 
